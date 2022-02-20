5aa4afd27e4f194c6da7edef525a670390f070c2
Boxfriend <cawills394@gmail.com>
2022-02-20 14:30:16 -0500

    this counts as completing the challenge, right?

---
+++ DeulingTraditions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DeulingTraditions
+{
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("I really don't want to have to bring one of the other projects into this one");
+            Console.WriteLine("I already split most of my projects into multiple files with namespaces anyway");
+            Console.WriteLine("This will serve as my solution since I have shown I know how to create a traditional entry point.");
+        }
+    }
+}
+
+//Answer to challenge question: i could go either way for traditional entry vs top-level statements. The top level statements are certainly convenient and easier to write
+//but I have also been coding long enough that I'm used to the traditional entry point
