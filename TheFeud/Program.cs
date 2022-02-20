fdf4b05db7a40c86bc7cc8d7026d517afd66c5d5
Boxfriend <cawills394@gmail.com>
2022-02-20 14:24:14 -0500

    oh boy, namespaces

--- TheFeud/Program.cs
+++ TheFeud/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IField;
+using McDroid;
+using IPig = IField.Pig;
+using MPig = McDroid.Pig;
+
+Console.Title = "The Feud";
+
+//ha ha jokes on me, i've been using namespaces this whole time
+var sheep = new Sheep();
+var cow = new Cow();
+var iPig = new IPig();
+var mPig = new MPig();
+
+
+namespace IField
+{
+    public class Sheep { }
+    public class Pig { }
+}
+
+namespace McDroid
+{
+    public class Cow { }
+    public class Pig { }
+}
