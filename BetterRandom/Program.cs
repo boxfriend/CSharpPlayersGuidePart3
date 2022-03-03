593488022b2efafcf28f951290d769845b6a088d
Boxfriend <cawills394@gmail.com>
2022-03-03 17:19:27 -0500

    better random, forgot to commit this previously whoops

---
+++ BetterRandom/Program.cs
@@ -0,0 +1,12 @@
+﻿using BetterRandom;
+
+Console.Title = "Better Random";
+
+var rng = new Random();
+Console.WriteLine($"Random double between 0 and 10 is {rng.NextDouble(10)}");
+Console.WriteLine($"Random string from list of strings {{ one, two, three }} : {rng.NextString("one","two","three")}");
+Console.WriteLine($"Coin flip result is {rng.CoinFlip()}");
+
+Console.WriteLine("Answer to challenge question: ");
+Console.WriteLine("It is much easier to work with extension methods than creating a derived class.");
+Console.WriteLine("Extension methods mean we don't need to start using a completely different type. Just to get the functionality of a couple of methods");
---
+++ BetterRandom/RandomExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace BetterRandom;
+internal static class RandomExtensions
+{
+    public static double NextDouble(this Random random, double max) => random.NextDouble() * max;
+
+    public static string NextString(this Random random, params string[] strings) => strings[random.Next(strings.Length)];
+
+    public static bool CoinFlip(this Random random, double trueChance = 0.5) => random.NextDouble() < trueChance;
+}
