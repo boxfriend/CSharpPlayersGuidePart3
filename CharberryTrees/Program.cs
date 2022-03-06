9448bbb6f06b429e842de5585600a26c77ec697a
Boxfriend <cawills394@gmail.com>
2022-03-05 21:21:13 -0500

    hooray for events existing now

---
+++ CharberryTrees/Program.cs
@@ -0,0 +1,47 @@
+﻿CharberryTree tree = new CharberryTree();
+var notifier = new Notifier(tree);
+var harvester = new Harvester(tree);
+
+while(true)
+    tree.MaybeGrow();
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+
+    public event Action? Ripened;
+
+    public void MaybeGrow()
+    {
+        if(_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+            
+    }
+}
+
+public class Notifier
+{
+    public Notifier (CharberryTree tree) => tree.Ripened += () => Console.WriteLine("A charberry fruit has ripened!");
+}
+
+public class Harvester
+{
+    public int HarvestedFruit { get; private set; }
+    private readonly CharberryTree _tree;
+    public Harvester (CharberryTree tree)
+    {
+        _tree = tree;
+        tree.Ripened += OnFruitRipened;
+    }
+
+    public void OnFruitRipened ()
+    {
+        HarvestedFruit++;
+        Console.WriteLine($"Total harvested: {HarvestedFruit}");
+        _tree.Ripe = false;
+    }
+}
