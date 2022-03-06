67d57f51f9c85934a8e98065fdbd0fcf951e0c8d
Boxfriend <cawills394@gmail.com>
2022-03-05 21:09:39 -0500

    completed the sieve

---
+++ TheSieve/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Title = "The Sieve";
+
+var value = 0;
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("Which of the following would you like?");
+    Console.WriteLine("1 - Even Checking Sieve");
+    Console.WriteLine("2 - Positive Checking Sieve");
+    Console.WriteLine("3 - Multiple of Ten Checking Sieve");
+    Console.Write("\nEnter a number from 1 to 3 indicating which option you would like: ");
+    var input = Console.ReadLine();
+    if (int.TryParse(input, out value))
+    {
+        break;
+    }
+}
+var sieve = GetSieveFromInput(value);
+while(true)
+{
+    Console.Write("Enter a number to check it with the sieve: ");
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        var adjective = sieve.IsGood(value) ? "good" : "bad";
+        Console.WriteLine($"{value} is {adjective}");
+    }
+}
+
+
+Sieve GetSieveFromInput(int input)
+{
+    return input switch
+    {
+        1 => Sieve.EvenChecker(),
+        2 => Sieve.PositiveChecker(),
+        3 => Sieve.TensChecker(),
+        _ => throw new NotImplementedException("Not a valid selection. Now you have to restart")
+    };
+}
+
+class Sieve
+{
+    private readonly Func<int, bool> _funk;
+    public Sieve (Func<int, bool> goodChecker) => _funk = goodChecker;
+    public bool IsGood (int number) => _funk.Invoke(number);
+
+    public static Sieve EvenChecker() => new (x => x % 2 == 0);
+    public static Sieve PositiveChecker () => new(x => x >= 0);
+    public static Sieve TensChecker () => new(x => x % 10 == 0);
+}
+
+
+//Answer to challenge question: This could easily have been solved using inheritance or even an interface where each class checks a specific condition
+//That approach is much messier though and can potentially lead to complicated inheritance hierarchies and managing multiple different types
+//This approach using delegates is much simpler to implement because we can easily change its behavior with a new instance and different delegate
+//rather than creating a whole new type just to change one behavior
