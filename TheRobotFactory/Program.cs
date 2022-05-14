e443536def18471fe86676d2ea6f43c50215147b
Boxfriend <cawills394@gmail.com>
2022-05-14 11:35:30 -0400

    done with dynamic objects. can i go back to static objects now

---
+++ TheRobotFactory/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Dynamic;
+
+var i = 1;
+for (;;)
+{
+    dynamic robot = new ExpandoObject();
+    robot.ID = i++;
+    Console.WriteLine($"You are producing robot #{robot.ID}");
+    GetName(robot);
+    GetSize(robot);
+    GetColor(robot);
+    foreach (KeyValuePair<string, object> obj in robot)
+    {
+        Console.WriteLine($"{obj.Key}: {obj.Value}");
+    }
+    SeparateLoops();
+}
+
+void SeparateLoops()
+{
+    for (var i = 0; i < 50; i++)
+        Console.Write("-");
+    Console.WriteLine();
+}
+
+void GetName(dynamic robot)
+{
+    if (!GetBooleanInput("Do you want to name this robot?")) return;
+
+    var name = "";
+    while (string.IsNullOrWhiteSpace(name))
+    {
+        Console.Write("What is its name? ");
+        name = Console.ReadLine();
+    }
+    robot.Name = name;
+}
+
+void GetSize(dynamic robot)
+{
+    if (!GetBooleanInput("Does this robot have a specific size?")) return;
+    robot.Width = GetIntInput("What is its width?");
+    robot.Height = GetIntInput("What is its height?");
+}
+
+void GetColor(dynamic robot)
+{
+    if (!GetBooleanInput("Does this robot have a specific color?")) return;
+    var input = "";
+    while(string.IsNullOrWhiteSpace(input))
+    {
+        Console.Write("What color? ");
+        input = Console.ReadLine(); //i know i *should* validate this input but honestly i just want to finish this
+    }
+    robot.Color = input;
+}
+
+bool GetBooleanInput(string message)
+{
+    var input = "";
+    do
+    {
+        Console.Write($@"{message} Y\N ");
+        input = Console.ReadLine();
+    } while (input.ToLower() is not ("y" or "n" or "yes" or "no"));
+
+    return input.ToLower() is "y" or "yes";
+}
+
+int GetIntInput(string message)
+{
+    var input = "";
+    do
+    {
+        Console.Write($@"{message} ");
+        input = Console.ReadLine();
+    } while (Convert.ToInt32(input) <= 0);
+    return Convert.ToInt32(input);
+}
---
+++ UniterOfAdds/Program.cs
@@ -0,0 +1,15 @@
+﻿//Answer to challenge question: since the method returns dynamic and both parameters are dynamic it is 100%
+//possible to enter in different types that may either throw an error or return an unexpected result
+//For example: providing two DateTime objects to the method throws an error
+
+var a = Add(1, 2);
+Console.WriteLine($"Ints: {a}");
+var b = Add(3.14, 3.14);
+Console.WriteLine($"Doubles: {b}");
+var c = Add("Hello", "World");
+Console.WriteLine($"Strings: {c}");
+var d = Add(DateTime.Now, TimeSpan.FromMinutes(420));
+Console.WriteLine($"DateTimes: {d}");
+
+
+static dynamic Add(dynamic a, dynamic b) => a + b;
