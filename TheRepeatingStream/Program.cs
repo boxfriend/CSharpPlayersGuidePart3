f94d423fe86129d30de5d1064605a1a6cd57d2a0
Boxfriend <cawills394@gmail.com>
2022-05-14 06:13:25 -0400

    The Repeating Stream completed

---
+++ TheRepeatingStream/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Press any key when you see matching numbers in the array");
+Thread.Sleep(1000);
+
+var recents = new RecentNumbers();
+var thread = new Thread(recents.GenerateRandoms);
+thread.Start();
+while (true)
+{
+    Console.ReadKey();
+    Console.WriteLine(recents.RecentInts[0] == recents.RecentInts[1]
+        ? "You correctly identified the repeat!"
+        : "This was not a repeat");
+}
+
+public class RecentNumbers
+{
+    public int[] RecentInts
+    {
+        get
+        {
+            lock(_recentInts)
+                return _recentInts;
+        }
+    }
+
+    private readonly int[] _recentInts = new int[2];
+    private readonly Random _rng = new();
+    public void GenerateRandoms()
+    {
+        while (true)
+        {
+            lock (_recentInts)
+            {
+                _recentInts[0] = _recentInts[1];
+                _recentInts[1] = _rng.Next(0,9);
+                Console.WriteLine($"{_recentInts[0]},{_recentInts[1]}");
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
