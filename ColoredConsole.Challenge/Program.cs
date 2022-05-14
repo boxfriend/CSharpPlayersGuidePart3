13053c5bd4d6b76ec07412309ddef2019a55e60d
Boxfriend <cawills394@gmail.com>
2022-05-14 15:54:01 -0400

    console of color

---
+++ ColoredConsole.Challenge/Program.cs
@@ -0,0 +1,4 @@
+﻿using ColoredConsole.Library; //Whoops naming the projects with a similar name caused some ambiguity
+
+var name = ColorConsole.Prompt("What is your name?"); 
+ColorConsole.WriteLine($"Hello {name}",ConsoleColor.Green);
---
+++ ColoredConsole.Library/ColorConsole.cs
@@ -0,0 +1,31 @@
+﻿using Console = System.Console;
+
+namespace ColoredConsole.Library;
+public static class ColorConsole
+{
+    public static string Prompt(string question)
+    {
+        var input = "";
+        do
+        {
+            Console.Write(question + " ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            input = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        } while (string.IsNullOrWhiteSpace(input));
+        return input;
+    }
+    public static void WriteLine(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    public static void Write(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
