7037d0f8b9de9d2845acc3bb86137c31fb2e45af
Boxfriend <cawills394@gmail.com>
2022-05-14 16:17:17 -0400

    the great humanizer

---
+++ TheGreatHumanizer/Program.cs
@@ -0,0 +1,7 @@
+﻿using Humanizer;
+
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(1).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(42).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(69).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(420).Humanize()}");
